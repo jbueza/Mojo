50030dc6cb89c4c13ba86c7805d5b5eb5589f3ed
Jaime Bueza <jbueza@gmail.com>
2010-11-11 21:56:52 -0800

    Fixed lint throwing errors with extra comma in Click.js and simplified markup in the examples.

--- examples/js/ExampleApp/Click.js
+++ examples/js/ExampleApp/Click.js
@@ -1,18 +1,18 @@
 Joose.Class('ExampleApp.Click', {
-    isa: 'Mojolite.Binder',
-    has: {
-      // This is required if you are to bind events the 'mojolite way' 
-        __eventMap: {init: [['context', "", 'click', 'testClick']]},
-    },
-    methods: {
-        testClick: function(e) {
-            console.log("testClick invoked")
-            return false;
-        }
-    },
-    after: {
-        initialize: function(props) {
-          // You can put 3rd party based binding of events here if you do not want to use __eventmap.
-        }
+  isa: 'Mojolite.Binder',
+  has: {
+  // This is required if you are to bind events the 'mojolite way' 
+    __eventMap: {init: [['context', "", 'click', 'testClick']]}
+  },
+  methods: {
+    testClick: function(e) {
+      alert("Hello there!");
+      return false;
     }
+  },
+  after: {
+    initialize: function(props) {
+      // You can put 3rd party based binding of events here if you do not want to use __eventmap.
+    }
+  }
 });
