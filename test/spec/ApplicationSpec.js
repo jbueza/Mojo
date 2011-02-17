cf45cb3cb98cd51011791687624e55056aeecb9b
jbueza <jbueza@gmail.com>
2011-02-17 03:31:00 -0800

    Fix grammar

--- test/spec/ApplicationSpec.js
+++ test/spec/ApplicationSpec.js
@@ -4,7 +4,7 @@ describe("Application", function() {
     
   });
 
-  it("should create be able to set a configuration key:value", function() { });
+  it("should be able to set a configuration setting", function() { });
   it("should be able to bind a dom element with a particular selector", function() { });
   it("should map controllers when invoking start", function() { });
   it("should fetch all plugins that the application is dependent on", function() { });
