eaae9bc1d5cf78ddf9cc7342f956b933ac9c9f9e
jbueza <jbueza@gmail.com>
2011-03-06 03:49:24 -0800

    Fix documemnation.

--- src/Request.js
+++ src/Request.js
@@ -1,4 +1,3 @@
-MOJO.define('Request', function() {
 /* 
  * Request 
  *
@@ -8,6 +7,8 @@ MOJO.define('Request', function() {
  *
  * @author Blast Radius (jbueza)
  */
+MOJO.define('Request', function() {
+  
 function Request(paramsObj, callerObj, eventObj, controllerObj) {
   this.paramsObj = paramsObj;
   this.callerObj = callerObj;
@@ -15,22 +16,36 @@ function Request(paramsObj, callerObj, eventObj, controllerObj) {
   this.controllerObj = controllerObj;
 };
 
-
+/* 
+ * Return the request's controller
+ */
 Request.prototype.getController = function() {
   return this.controllerObj;
 };
 
+/* 
+ * Returns the context DOM element
+ */
 Request.prototype.getContextElement = function() {
   return this.getController().getContextElement();
 };
+
+/* 
+ * Returns the object that invoked the request
+ */
 
 Request.prototype.getCaller = function() {
   return this.callerObj;
 };
 
+/* 
+ * Returns an event object that was generated from the user interaction
+ */
+
 Request.prototype.getEvent = function() {
   return this.eventObj;
 };
+
 window.Request = Request;
 return Request;
 });
