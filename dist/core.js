ab14a423f7ec14c3c782e15adf651674ed8db2b2
jbueza <jbueza@gmail.com>
2011-03-07 08:34:40 -0800

    Rename to _min instead of .min

--- dist/core.js
+++ dist/core.js
@@ -136,7 +136,6 @@
   window.MOJO = MOJO;
    
 })(window, document);
-MOJO.define('Request', function() {
 /* 
  * Request 
  *
@@ -146,6 +145,8 @@ MOJO.define('Request', function() {
  *
  * @author Blast Radius (jbueza)
  */
+MOJO.define('Request', function() {
+  
 function Request(paramsObj, callerObj, eventObj, controllerObj) {
   this.paramsObj = paramsObj;
   this.callerObj = callerObj;
@@ -153,22 +154,36 @@ function Request(paramsObj, callerObj, eventObj, controllerObj) {
   this.controllerObj = controllerObj;
 };
 
-
+/* 
+ * Return the request's controller
+ */
 Request.prototype.getController = function() {
   return this.controllerObj;
 };
 
+/* 
+ * Returns the context DOM element
+ */
 Request.prototype.getContextElement = function() {
   return this.getController().getContextElement();
 };
+
+/* 
+ * Returns the object that invoked the request
+ */
 
 Request.prototype.getCaller = function() {
   return this.callerObj;
 };
 
+/* 
+ * Returns an event object that was generated from the user interaction
+ */
+
 Request.prototype.getEvent = function() {
   return this.eventObj;
 };
+
 window.Request = Request;
 return Request;
 });MOJO.define('Controller', function() {  
@@ -209,7 +224,9 @@ Controller.prototype.initialize = function(context, controllerName, params) {
     if (scope == "context") root = $(context);    
     
     $(root).delegate(selector, eventName, function(evt) {
+ 
       var requestObj = new Request({}, this, evt, self);
+      
       if (typeof self.before != 'undefined' && typeof self.before[commandName] != 'undefined') self.before[commandName].call(self, requestObj);
       self.methods[commandName].call(self, requestObj);
       if (typeof self.after != 'undefined' && typeof self.after[commandName] != 'undefined') self.after[commandName].call(self, requestObj);
