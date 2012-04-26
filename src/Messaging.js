7c49c1301bbd6f515ee07dd743d10e59e00e85db
Jaime Bueza <jbueza@gmail.com>
2012-04-26 01:17:41 -0700

    Updating documentation on the Messaging class.

--- src/Messaging.js
+++ src/Messaging.js
@@ -8,18 +8,35 @@ var $ = jQuery
   , messageStore = $({})
   , Messaging = function() {};
 
-Messaging.subscribe = function() {
-  messageStore.bind.apply( messageStore, arguments );
-};
+  /*
+   * Subscribes to a particular messaging topic
+   *
+   * @param topic {String} - Represents a topic for messages to get pushed to
+   * @param callback {Function} - A callback to execute when the topic receives a message
+   */
+  Messaging.subscribe = function() {
+    messageStore.bind.apply( messageStore, arguments );
+  };
+  
+  /* 
+   * Unsubscribes to a particular topic
+   * 
+   * @param topic {String} - A Messaging topic to unsubscribe from
+   */
+  Messaging.unsubscribe = function() {
+    messageStore.unbind.apply( messageStore, arguments );
+  };
 
-Messaging.unsubscribe = function() {
-  messageStore.unbind.apply( messageStore, arguments );
-};
+ /* 
+  * Publishes a message to a particular topic
+  * @param topic {String} - A Messaging Topic to push a message to
+  * @param params {Object} - A JSON object that gets passed into the subscription topic
+  * Example: mojo.Messaging.publish("MyAwesomeTopic", parameters)
+  */
+  Messaging.publish = function() {
+    messageStore.trigger.apply( messageStore, arguments );
+  };
 
-Messaging.publish = function() {
-  messageStore.trigger.apply( messageStore, arguments );
-};
+  mojo.Messaging = Messaging;
 
-window.mojo.Messaging = Messaging;  
-if (window.MOJO) window.MOJO.Messaging = Messaging;
 });
