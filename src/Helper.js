a871525b48578e5aa2057476f1dfa00e8bee095f
Jaime Bueza <jbueza@gmail.com>
2012-06-29 19:38:33 -0700

    #6 Add helper file.

---
+++ src/Helper.js
@@ -0,0 +1,12 @@
+/* 
+* Service
+* @author Jaime Bueza
+* A place where helpers can help developers
+*/
+mojo.define('mojo.helper', function($) {
+  
+  var helpers = {};
+  
+  
+  mojo.helper = helpers;
+});
